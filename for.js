4a80f46223c378c7a81ea75d713436484da70247
daniel_pacas <cristian2017daniel@gmail.com>
2024-08-27 20:38:03 -0600

    Add files via upload

---
+++ for.js
@@ -0,0 +1,42 @@
+// Cristian Daniel Pacas Guevara, U20240388
+//EJERCICIO 1
+for (i= 0; i<=9; i+=2){console.log("El numero par positivo es:" + i + "\n");
+}
+
+//EJERCICIO 2
+let a = 5;
+for (j=1; j<=a; j++){
+    console.log("*".repeat (j) + "\n");
+}
+
+//EJERCICIO 3
+let suma = 0;
+for (j=0; j<=5; j++){suma+=j;} {
+    console.log("La sumatoria de los numeros del 0 al " + "5" + " es: " + suma);
+}
+
+//EJERCICIO 4
+
+let numeros = [5,9,10,12];
+let sumatoria = 0;
+for (a=0; a < numeros.length; a++){
+     sumatoria+=numeros[a];
+    }
+     console.log("La sumatoria de los valores ingresados es de: " + sumatoria);
+ 
+const mediaaritmetica = sumatoria/4;
+ console.log("La media aritmetica de los valores ingresados es de: " + mediaaritmetica);
+
+
+//EJERCICIO 5
+let numero = 3;
+for (t=1; t<=10; t++) {
+    console.log (numero + " x " + t + " = " + numero*t  + " \n ");}
+
+
+
+
+
+
+
+
